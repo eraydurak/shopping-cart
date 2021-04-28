ef7777cf8bebb196a7b6f5b8a5c0f3f2b6924f26
Eray Durak <eraydurak1@outlook.com>
2021-04-29 01:35:08 +0300

    Sendind basket and product props

--- src/App.js
+++ src/App.js
@@ -2,22 +2,51 @@ import { useState, useEffect } from "react";
 import "./App.scss";
 import Header from "./components/Header";
 import Product from "./components/Product";
+import Basket from "./components/Basket";
 import products from "./products.json";
+import BasketItem from "./components/BasketItem";
 
 const App = () => {
-  const [money, setMoney] = useState(1000);
+  const [money, setMoney] = useState(1000000);
   const [basket, setBasket] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const resetBasket = () => {
+    setBasket([]);
+  };
 
   useEffect(() => {
-    console.log(basket);
+    setTotal(
+      basket.reduce((acc, item) => {
+        return (
+          acc +
+          item.amount * products.find((product) => product.id === item.id).price
+        );
+      }, 0)
+    );
   }, [basket]);
 
   return (
     <>
-      <Header money={money} />
+      <Header total={total} money={money} />
       {products.map((product) => (
-        <Product basket={basket} setBasket={setBasket} product={product} />
+        <Product
+          total={total}
+          money={money}
+          key={product.id}
+          basket={basket}
+          setBasket={setBasket}
+          product={product}
+        />
       ))}
+      {total > 0 && (
+        <Basket
+          resetBasket={resetBasket}
+          total={total}
+          basket={basket}
+          products={products}
+        />
+      )}
     </>
   );
 };
