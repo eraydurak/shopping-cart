77aedb04a21814a763defe7df261959e10565af6
Eray Durak <eraydurak1@outlook.com>
2021-04-29 01:35:42 +0300

    Sending props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./Header.scss";
+import { moneyFormat } from "./../helpers";
 
-const Header = ({ money }) => {
+const Header = ({ money, total }) => {
   return (
     <>
-      <p>You have {money} &#x20BA; </p>
+      <div>
+        {total > 0 && money - total !== 0 && (
+          <div>Harcayacak {moneyFormat(money - total)} TL kaldı</div>
+        )}
+        {total === 0 && (
+          <div>Harcamak için {moneyFormat(money - total)} TL paranız var</div>
+        )}
+        {money - total === 0 && <div>Paran bitti</div>}
+      </div>
     </>
   );
 };
