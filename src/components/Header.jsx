737794a6c8c06057f8dd7012825f42ff08fa044a
Eray Durak <eraydurak1@outlook.com>
2021-06-06 21:56:44 +0300

    Added new elemenets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,12 @@ const Header = ({ money, total }) => {
     <>
       <div>
         {total > 0 && money - total !== 0 && (
-          <div>Harcayacak {moneyFormat(money - total)} TL kaldı</div>
+          <div className="money-spend">Harcayacak <span>{moneyFormat(money - total)}</span> TL kaldı</div>
         )}
         {total === 0 && (
-          <div>Harcamak için {moneyFormat(money - total)} TL paranız var</div>
+          <div className="money-spend">Harcamak için <span>{moneyFormat(money - total)}</span> TL paranız var</div>
         )}
-        {money - total === 0 && <div>Paran bitti</div>}
+        {money - total === 0 && <div className="money-spend">Paran bitti</div>}
       </div>
     </>
   );
